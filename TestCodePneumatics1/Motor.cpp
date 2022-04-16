2799c8c38921dfd06f64f90a8c3390ca1a44e0b9
Rjc3002 <rjc3002@rit.edu>
2022-04-16 19:36:59 -0400

    Motor class structure
    Fleshed out Motor Class header and source. Added test file to experiment with encoder input.
    -RC

--- TestCodePneumatics1/Motor.cpp
+++ TestCodePneumatics1/Motor.cpp
@@ -1,31 +1,65 @@
 #include "Motor.h"
 
-  /*int motorPin;
-  int escPin;
-  int encoderPinA;
-  int encoderPinB;*/
-    
-  Motor::Motor(int motorPin,int escPin, int encoderPinA, int encoderPinB); //construct motor object
+  Motor::Motor (int motorPin, int encoderPinA, int encoderPinB)
+  : motorPin(motorPin), encoderPinA(encoderPinA), encoderPinB(encoderPinB){
+      attach(motorPin);
+      arm();
+      pinMode(encoderPinA, input);
+      pinMode(encoderPinB, input);
+
+  } //construct motor object
+
   void Motor::Run(int speedPcnt, int timeSec){
-    
+      if(speedPcnt <= 100 && speedPcnt >= -100){
+        this.setSpeed(speedPcnt);
+        }
+      delay(timeSec * 1000);
   }//Run motor
   
   void Motor::Run(int dir, int deg, int speedPcnt){
-    
+    int speed;
+    int time;
+    if(dir == 0){
+        speed = -1 * abs(speedPcnt);
+    }
+    else{
+        speed = abs(speedPcnt);
+    }
+    //Implement conversion from desired degrees to speed+time given a certain speed*************
+    //***************************
+
   }//Run motor based on pos
+
+  void Motor::tuneESC(double low, double high){
+      //High forward
+      int angle = map(99, -100, 100, low, high); //Sets servo positions to different speeds
+      this.write(angle);
+      delay(7000);
+
+      //High reverse
+      int angle = map(-99, -100, 100, mapLow, mapHigh); //Sets servo positions to different speeds
+      this.write(angle);
+      delay(10000);
+  }//Setup ESC range
   
   int Motor::getDir(){
-    
+
   }//Get directional feedback from encoder
   
   int Motor::getVel(){
-    
+
   }//Get velocity feedback from encoder
   
-  void Motor::setspeed(Servo M, int speed){
-    
+  void Motor::setSpeed(int speed){
+    int angle = map(speed, -100, 100, mapLow, mapHigh); //Sets servo positions to different speeds ESC1.write(angle);
+    //With full mapping [[map(speed, -100, 100, 0, 180)]], the max servo speeds are -88%->88%.
+
+    this.write(angle);
   }//Set motor speed and map
 
+  void Motor::arm(){
+      setSpeed(0);
+  }
 
 
  
--- TestCodePneumatics1/Motor.h
+++ TestCodePneumatics1/Motor.h
@@ -5,21 +5,33 @@
 #include "Arduino.h"
 #include <Servo.h>
 
-class Motor {
+static double mapLow = 10.8;
+static double mapHigh = 169.2;
+
+class Motor : public Servo {
   public:
-    Motor(int motorPin,int escPin, int encoderPinA, int encoderPinB); //construct motor object
-    void Run(int speedPcnt, int timeSec); //Run motor
-    void Run(int dir, int deg, int speedPcnt); //Run motor based on pos
+    Motor(int motorPin, int encoderPinA, int encoderPinB); //construct motor object
+
+    void Run(int speedPcnt, int timeSec); //Run motor for a % speed and a time in seconds
+
+    void Run(int dir, int deg, int speedPcnt); //Run motor based on pos; dir = 0/1, 0=-% 1=+%
+
+    void tuneESC(double low, double high); //Setup ESC range
+
     int getDir(); //Get directional feedback from encoder
+
     int getVel(); //Get velocity feedback from encoder
+
     
 
-    private:
+  private:
     int motorPin;
-    int escPin;
     int encoderPinA;
     int encoderPinB;
-    void setspeed(Servo M, int speed);
+
+    void setSpeed(int speed);
+
+    void arm();
     
 };
 
