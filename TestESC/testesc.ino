bba3aac0917dabae51bcf1db470c0e522ac3e1c2
emb6650 <78239325+EvanBukofsky@users.noreply.github.com>
2022-10-30 21:07:43 -0400

    Update testesc.ino
    rejecting invalid characters from serial port - possibly newline or null

--- TestESC/testesc.ino
+++ TestESC/testesc.ino
@@ -1,17 +1,18 @@
- #include <Servo.h>
+#include <Servo.h>
 
 
 
-int value = 2000; //tialize the variables you need
+int value = 900; //low threshold
+int temp; //used to filter null characters
 
 
-Servo firstESC; // you can control 2 or more servos simultaneously
+Servo firstESC; 
 
 
 void setup() {
 
  
- firstESC.attach(13);   
+ firstESC.attach(13);   //pin 13
 
  Serial.begin(9600);   // start serial at 9600 baud
 
@@ -22,14 +23,24 @@ void setup() {
 void loop() {
 
 
-//First connect your ESC WITHOUT arming. Then open the serial and follow the instructions.
+/*
+1) Run Arduino Code
+2) Turn on ESC power supply
+3) Wait for beeps, enter high threshold, wait again, enter low threshold
+*/
 
  if(Serial.available()>0){ 
 
-   value = Serial.parseInt();   // parse an integer from serial
+   temp = Serial.parseInt();   // parse an integer from serial
  }
  
+ if((temp>=900) && (temp<=2000)) //checking for invalid characters
+ {
+  value = temp;
+ }
+ 
+   Serial.println(value);   //user feedback
+   firstESC.writeMicroseconds(value);
 
- Serial.println(value);
- firstESC.writeMicroseconds(value);
+ delay(100);
 }
