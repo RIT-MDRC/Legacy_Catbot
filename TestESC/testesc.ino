424d1ba8dbd11ac0dee30149f58b6eb8748a42d1
EvanBukofsky <emb6650@rit.edu>
2022-11-03 01:20:10 -0400

    starting sequence runs through all possible speed values

--- TestESC/testesc.ino
+++ TestESC/testesc.ino
@@ -1,8 +1,13 @@
 #include <Servo.h>
 
+#define LOWERLIMIT 900
+#define MIDDLE 1500 // middle/starting value
+#define UPPERLIMIT 2000
 
+#define START 3 //garbage nonzero value - enter to begin testing process
+                //could and should be refined
 
-int value = 900; //low threshold
+int value = MIDDLE; 
 int temp; //used to filter null characters
 
 
@@ -16,31 +21,74 @@ void setup() {
 
  Serial.begin(9600);   // start serial at 9600 baud
 
+ //set middle value ...........................
+  Serial.println("middlevalue");
+  
+  firstESC.writeMicroseconds(MIDDLE);
+  Serial.println(MIDDLE);   //user feedback
+  while(value != START){
+    value = Serial.parseInt(); 
+    Serial.println("enter 3 to start");
+    delay(100);
+  }
+//.............................................
 
+
+//updown.......................................
+  for(int i=MIDDLE;i<=UPPERLIMIT;i+=20){
+    firstESC.writeMicroseconds(i);
+    Serial.println(i);
+    delay(100);
+  }
+  delay(2000); //pause at high thresh
+  
+  for(int i=UPPERLIMIT;i>=LOWERLIMIT;i-=20){
+    firstESC.writeMicroseconds(i);
+    Serial.println(i);
+    delay(100);
+  }
+  delay(2000); //pause at low thresh
+
+  for(int i=LOWERLIMIT;i<=MIDDLE;i+=20){ //reset to middle position before entering manual mode
+    firstESC.writeMicroseconds(i);
+    Serial.println(i);
+    delay(100);
+  }
+  delay(1000);
+
+  value = MIDDLE;
+
+//.........................    
 }
+
+
 
 
 void loop() {
 
+//enter write values manually in infinite loop
 
-/*
-1) Run Arduino Code
-2) Turn on ESC power supply
-3) Wait for beeps, enter high threshold, wait again, enter low threshold
-*/
-
- if(Serial.available()>0){ 
-
-   temp = Serial.parseInt();   // parse an integer from serial
- }
+ temp = Serial.parseInt();
  
- if((temp>=900) && (temp<=2000)) //checking for invalid characters
+ 
+ if(temp>=1) //checking for invalid characters
  {
   value = temp;
  }
+ 
+/*
+ if(temp == 'k')//killswitch
+ {
+  while(1)
+  {
+    firstESC.writeMicroseconds(MIDDLE);
+    value = MIDDLE;
+  }
+ }*/
  
    Serial.println(value);   //user feedback
    firstESC.writeMicroseconds(value);
 
  delay(100);
 }
+
