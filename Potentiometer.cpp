12237b0faa4db8458ea3c74ee8bfef2d41ec11f6
thatnoobles <danildonchuk@gmail.com>
2022-11-12 16:43:06 -0500

    separated header into h and cpp files -dan

---
+++ Potentiometer.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include "Potentiometer.h"
+
+// Represents a potentiometer that is attached to a hip of the robot.
+class Potentiometer
+{
+public:
+  Potentiometer(int _pin, int _maxDeg, int _maxValue = 1023)
+  {
+    pin = _pin;
+    maxDeg = _maxDeg;
+    maxValue = _maxValue;
+
+    pinMode(pin, INPUT);
+  };
+
+  // Converts raw data to a rotational degree value.
+  // Starts at zero, and increases as potentiometer is rotated counterclockwise.
+  float getReading()
+  {
+    float potVar = analogRead(pin);
+    return potVar / maxValue * maxDeg;
+  }
+
+  // Gets current degree reading as an integer value.
+  int getReadingInt() { return (int)ceil(getReading()); }
+
+private:
+  int pin;          // Pin that will read potentiometer values
+  int maxDeg;       // Maximum rotation of the potentiometer (degrees)
+  int maxValue;     // Maximum raw output value of the potentiometer (usually is 1023)
+};
--- Potentiometer.h
+++ Potentiometer.h
@@ -1,36 +1,20 @@
-#ifndef Potentiometer_h
-#define Potentiometer_h
+// Potentiometer reader
+// Written by: Dan and Hiro
 
-#include <Arduino.h>
-
-// Represents a potentiometer that is attached to a hip of the robot.
 class Potentiometer
 {
 public:
-  Potentiometer(int _pin, int _maxDeg, int _maxValue = 1023)
-  {
-    pin = _pin;
-    maxDeg = _maxDeg;
-    maxValue = _maxValue;
-
-    pinMode(pin, INPUT);
-  };
+  Potentiometer(int _pin, int _maxDeg, int _maxValue = 1023);
 
   // Converts raw data to a rotational degree value.
   // Starts at zero, and increases as potentiometer is rotated counterclockwise.
-  float getReading()
-  {
-    float potVar = analogRead(pin);
-    return potVar / maxValue * maxDeg;
-  }
+  float getReading();
 
   // Gets current degree reading as an integer value.
-  int getReadingInt() { return (int)ceil(getReading()); }
+  int getReadingInt();
 
 private:
   int pin;          // Pin that will read potentiometer values
   int maxDeg;       // Maximum rotation of the potentiometer (degrees)
   int maxValue;     // Maximum raw output value of the potentiometer (usually is 1023)
 };
-
-#endif
