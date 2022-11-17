6a049770ed97b65ebb8475d62c53ec6ce54d0bb7
hiromon0125 <hiroto125takeuchi@gmail.com>
2022-11-17 15:02:15 -0500

    fin: incorporate both potentiometer and motors

--- src/ArduinoWorking.ino
+++ src/ArduinoWorking.ino
@@ -13,7 +13,7 @@ INSTRUCTIONS:
 
 #include <Servo.h>
 #include "Motor.h"
-//#include "Potentiometer.h"
+#include "Potentiometer.h"
 
 #define LOWERLIMIT 900
 #define MIDDLE 1500 // middle/starting value
@@ -25,18 +25,26 @@ INSTRUCTIONS:
 #define APOT A5
 #define BPOT A4
 
+#define MAXDEG 285.00
+
 #define STOP 1 // garbage value - enter in serial monitor to begin testing process
                // could and should be refined
 
 // Messing around with pointers because we are cool like that :D
 Motor* aMotor = NULL;
 Motor* bMotor = NULL;
+Potentiometer* aPot = NULL;
+Potentiometer* bPot = NULL;
 
+// Potentiometer* aPot = NULL;
+// Potentiometer* bPot = NULL;
 
 void setup()
 {
   aMotor = new Motor(AMOTORPIN, LOWERLIMIT, UPPERLIMIT, MIDDLE);
   bMotor = new Motor(BMOTORPIN, LOWERLIMIT, UPPERLIMIT, MIDDLE);
+  aPot = new Potentiometer(APOT, MAXDEG);
+  bPot = new Potentiometer(BPOT, MAXDEG);
   Serial.begin(9600); // start serial at 9600 baud
   while (!Serial)
   {
@@ -44,24 +52,53 @@ void setup()
   delay(1000);
   Serial.write("starting program\n");
   delay(1000);
+  String aDegree = String(aPot -> getReading());
+  String bDegree = String(bPot -> getReading());
+  Serial.println(aDegree);
+  Serial.println(bDegree);
+
   // motorA.arm();
-  aMotor -> run(40, 5);
-  bMotor -> run(40, 5);
+  aMotor -> runCall(40);
+  bMotor -> runCall(40);
+  
+  while ((!checkPot(aPot, 30.0, MAXDEG))||(!checkPot(bPot, 30.0, MAXDEG))){}
+  aMotor -> arm();
+  bMotor -> arm();
+  // delay(1000);
 
-
-
-
-  delete aMotor;
-  delete bMotor;
-  aMotor = NULL;
-  bMotor = NULL;
+  // Make sure that the program has successfully ended before halting to prevent possible chance of memory leak
+  end();
 }
 
 void loop()
 {
-  Serial.write("running loop\n");
+  end();
+  delay(100);
+}
 
-  delay(10);
+void end(){
+  if (!aMotor || !bMotor || !aPot || !bPot) {
+    Serial.write("Program succesfully ended.\n");
+    return;
+  }
+  Serial.write("ending program...\n");
+  delay(1000);
+  delete aMotor;
+  delete bMotor;
+  delete aPot;
+  delete bPot;
+  aMotor = NULL;
+  bMotor = NULL;
+  aPot = NULL;
+  bPot = NULL;
+}
+
+bool checkPot(Potentiometer* pot, float lowerRange, float upperRange){
+  float reading = pot -> getReading();
+  Serial.println("checking pot " + String(reading));
+  bool lowerCheck = (lowerRange <= reading);
+  bool upperCheck = (reading <= upperRange);
+  return (lowerCheck && upperCheck);
 }
 
 
--- src/Motor.cpp
+++ src/Motor.cpp
@@ -1,6 +1,6 @@
 #include "Motor.h"
 
-#define DELAY 100   // ms of delay between writing to ESCs
+#define DELAY 100 // ms of delay between writing to ESCs
 
 Motor::Motor(int _motorPin, int _mapLow, int _mapHigh, int _mapMiddle)
 {
@@ -22,40 +22,58 @@ void Motor::arm()
 void Motor::run(int speedPercent, double seconds)
 {
   // Only allow speed percentages in the range [-100%, 100%]
-  if (speedPercent > 100 && speedPercent < -100) { return; }
+  if (speedPercent > 100 && speedPercent < -100)
+  {
+    return;
+  }
 
   // If user sets 0% speed, they're essentially just arming the motor
   if (speedPercent == 0)
   {
-     arm();
-     return;
+    arm();
+    return;
   }
 
   // Middle value might not be in the actual center, so map() will have different
   // ranges based on where the "middle" is
   int velocity;
 
-  if (speedPercent > 0) { velocity = map(speedPercent, 0, 100, mapMiddle, mapHigh); }
-  else { velocity = map(speedPercent, -100, 0, mapLow, mapMiddle); }
-  
-  Serial.print("Sending ");
-  Serial.print(velocity);
-  Serial.print(" ");
+  if (speedPercent > 0)
+  {
+    velocity = map(speedPercent, 0, 100, mapMiddle, mapHigh);
+  }
+  else
+  {
+    velocity = map(speedPercent, -100, 0, mapLow, mapMiddle);
+  }
 
   esc.writeMicroseconds(velocity);
-  delay(seconds*1000);
+  delay(seconds * 1000);
   arm();
 }
 
 void Motor::runCall(int speedPercent)
 {
-  if (speedPercent > 100 && speedPercent < -100) { return; }
-  if (speedPercent == 0) { arm(); return; }
+  if (speedPercent > 100 && speedPercent < -100)
+  {
+    return;
+  }
+  if (speedPercent == 0)
+  {
+    arm();
+    return;
+  }
 
   int velocity;
 
-  if (speedPercent > 0) { velocity = map(speedPercent, 0, 100, mapMiddle, mapHigh); }
-  else { velocity = map(speedPercent, -100, 0, mapLow, mapMiddle); }
+  if (speedPercent > 0)
+  {
+    velocity = map(speedPercent, 0, 100, mapMiddle, mapHigh);
+  }
+  else
+  {
+    velocity = map(speedPercent, -100, 0, mapLow, mapMiddle);
+  }
 
   esc.writeMicroseconds(velocity);
 }
